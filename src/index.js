4456a629ae18f8093eb32195a0b2c9c7db604b28
kelibst <kboster17@gmail.com>
2020-09-03 07:27:18 +0000

    solved inability to start bug

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { config } from './assets/script/config/config';
 
 const Game = new Phaser.Game(config);
 window.focus();
-
+config.resize();
+window.addEventListener('resize', config.resize, false);
 
 document.addEventListener('DOMContentLoaded', (e) => {
-  config.resize();
-  window.addEventListener('resize', config.resize, false);
+  
 });
