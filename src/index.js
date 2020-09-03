b61c9d4e49fd0dd2a641661256f4da1734dca377
kelibst <kboster17@gmail.com>
2020-09-03 09:47:47 +0000

    added responsiveness

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 /* eslint-disable  no-unused-vars */
 
 import Phaser from 'phaser';
-
 import config from './assets/script/config/config';
 
-
 const Game = new Phaser.Game(config);
-window.focus();
-config.resize();
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  window.addEventListener('resize', config.resize, false);
-});
